e019282228d3c985792f528e52bc7caa9bedd3fe
Andy Antes <aantes@tableau.com>
2019-10-30 20:02:34 -0700

    Added some more detail to the cards

--- client/src/components/ReadingCard.js
+++ client/src/components/ReadingCard.js
@@ -1,37 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import axios from "axios";
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 275,
+    minWidth: 275,
     margin: 10
+  },
+  media: {
+    height: 400
   }
 });
 
 const ReadingCard = props => {
   const classes = useStyles();
-  const [bookInfo, setBookInfo] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get("/api/reading")
-      .then(res => {
-        console.log(res);
-        setBookInfo(res.data.length);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
 
   return (
     <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        title={props.title}
+        image={props.media}
+      />
       <CardContent>
-        <Typography variant="h5">I'm reading {bookInfo} books!</Typography>
+        <Typography variant="h5">{props.title}</Typography>
+        <Typography variant="subtitle1">{props.subtitle}</Typography>
       </CardContent>
     </Card>
   );
--- client/src/components/ReadingPage.js
+++ client/src/components/ReadingPage.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import ReadingCard from "./ReadingCard";
+import axios from "axios";
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 275,
-    margin: 10
+    margin: 40
+  },
+  readingPage: {
+    display: "flex",
+    flexDirection: "row"
   }
 });
 
-const renderCards = () => {
-  const ids = [1, 2, 3, 4, 5];
-  const cards = ids.map(id => <ReadingCard key={id} />);
+const renderCards = bookshelf => {
+  const cards =
+    bookshelf &&
+    bookshelf.map(book => (
+      <ReadingCard
+        media={book.volumeInfo.imageLinks.thumbnail}
+        title={book.volumeInfo.title}
+        subtitle={book.volumeInfo.subtitle}
+      />
+    ));
 
   return cards;
 };
 
 const ReadingPage = props => {
   const classes = useStyles();
+  const [bookshelf, setBookshelf] = useState(null);
 
-  return <div>{renderCards()}</div>;
+  useEffect(() => {
+    axios
+      .get("/api/reading")
+      .then(res => {
+        setBookshelf(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return <div className={classes.readingPage}>{renderCards(bookshelf)}</div>;
 };
 
 export default ReadingPage;
