942ade027bde1fec9f609abad3419976da2835d7
Andy Antes <aantes@tableau.com>
2019-11-23 14:26:24 -0800

    Added skills to boxes

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,15 +9,39 @@ const Header = props => {
             <div className='box bg-blue'>
                 <i class="fas fa-cogs fa-3x"></i>
                 <h3>Backend Development</h3>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>Python</li>
+                    <li>Django</li>
+                    <li>Java</li>
+                </ul>
             </div>
             <div className='box bg-brown text-secondary'>
                 <i class="fas fa-code fa-3x"></i>
                 <h3>Frontend Development</h3>
+                <ul>
+                    <li>Javascript</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>React</li>
+                    <li>&nbsp;</li>
+                    
+                </ul>
             </div>
             <div className='box bg-blue'>
                 <i class="fas fa-sitemap fa-3x"></i>
                 <h3>DevOps | CI/CD | Testing</h3>
+                <ul>
+                    <li>Amazon Web Services</li>
+                    <li>GitLab CI</li>
+                    <li>Bamboo</li>
+                    <li>Selenium</li>
+                    <li>Docker</li>
+                    
+                </ul>
             </div>
+            <div className="clr"></div>
 
         
     </section>
